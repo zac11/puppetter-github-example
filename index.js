ddc46d435a910b05599e05c27d79ab5aeea29f73
zac11 <ydv.rahul09@gmail.com>
2019-01-22 10:37:31 +0530

    Changed for github

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ async function run() {
         args :['--start-fullscreen']
     });
     const page = await browser.newPage();
+    await page.setViewport({width:1440,height:900});
 
     await page.goto('https://github.com');
     await page.screenshot({path: 'screenshots/github.png'});
@@ -30,60 +31,74 @@ async function run() {
 
     await page.waitForNavigation();
 
-    await page.goto(searchUrl);
+    await page.goto("https://github.com/trending");
 
-    await page.waitFor(2*1000);
+    await page.waitFor(5*1000);
 
+    const data = await page.evaluate(()=>{
+        const tds =Array.from(document.querySelectorAll('.explore-content ol li div h3'));
+        return tds.map(td => td.textContent);
+    });
 
-    const LIST_USERNAME_SELECTOR = '#user_search_results > div.user-list > div:nth-child(INDEX) > div.d-flex > div > a';
-    const LIST_EMAIL_SELECTOR = "#user_search_results > div.user-list > div:nth-child(INDEX) > div.d-flex > div > ul > li:nth-child(2) > a";
-    const LENGTH_SELECTOR_CLASS = 'user-list-item';
-    const numofPages = await getNumofPages(page);
-    console.log('There are a total of ' + numofPages + ' with this username');
+    const stars = await page.evaluate(()=>{
+        const stars = Array.from(document.querySelectorAll('.explore-content ol li div:nth-child(4) a'));
+        return stars.map(star=>star.textContent);
+        
+    })
 
-    for(let h=1;h<=5;h++){
-        //you can replace this with numofPages to go to all the pages.
-        let pageURL = searchUrl+ '&p='+ h;
-        await page.goto(pageURL);
-
-        let listLength = await page.evaluate((sel)=>{
-            return document.getElementsByClassName(sel).length;     
-        },LENGTH_SELECTOR_CLASS);
-        console.log('List Length', listLength);
-
-        await page.waitFor(5*1000);
+    console.log(`The top repositories are ${data}`);
+    console.log(`The top repoitories have ${stars} stars`);
 
 
-        for(let i=1;i<=listLength;i++){
+    // const LIST_USERNAME_SELECTOR = '#user_search_results > div.user-list > div:nth-child(INDEX) > div.d-flex > div > a';
+    // const LIST_EMAIL_SELECTOR = "#user_search_results > div.user-list > div:nth-child(INDEX) > div.d-flex > div > ul > li:nth-child(2) > a";
+    // const LENGTH_SELECTOR_CLASS = 'user-list-item';
+    // const numofPages = await getNumofPages(page);
+    // console.log('There are a total of ' + numofPages + ' with this username');
 
-            //replace the INDEX with the actual number;
-            let userName = LIST_USERNAME_SELECTOR.replace('INDEX',i);
-            //console.log(userName);
-            let email = LIST_EMAIL_SELECTOR.replace('INDEX',i);
-            //console.log(email);
+    // for(let h=1;h<=5;h++){
+    //     //you can replace this with numofPages to go to all the pages.
+    //     let pageURL = searchUrl+ '&p='+ h;
+    //     await page.goto(pageURL);
 
-            let usernameSelector = LIST_USERNAME_SELECTOR.replace("INDEX", i);
-            let emailSelector = LIST_EMAIL_SELECTOR.replace("INDEX", i);
+    //     let listLength = await page.evaluate((sel)=>{
+    //         return document.getElementsByClassName(sel).length;     
+    //     },LENGTH_SELECTOR_CLASS);
+    //     console.log('List Length', listLength);
+
+    //     await page.waitFor(5*1000);
 
 
-            let usernameofperson = await page.evaluate((sel)=>{
-                return document.querySelector(sel).getAttribute('href').replace('/', '');
-            }, userName);
+    //     for(let i=1;i<=listLength;i++){
 
-            let emailofUser = await page.evaluate((sel)=>{
-                let element = document.querySelector(sel);
-                return element ? element.innerHTML : null;
-            }, email);
+    //         //replace the INDEX with the actual number;
+    //         let userName = LIST_USERNAME_SELECTOR.replace('INDEX',i);
+    //         //console.log(userName);
+    //         let email = LIST_EMAIL_SELECTOR.replace('INDEX',i);
+    //         //console.log(email);
 
-            //if there is no email for user, then continue
-            if (!emailofUser)
-                continue;
+    //         let usernameSelector = LIST_USERNAME_SELECTOR.replace("INDEX", i);
+    //         let emailSelector = LIST_EMAIL_SELECTOR.replace("INDEX", i);
 
-            console.log(usernameofperson, ' -> ', emailofUser);
-        }
+
+    //         let usernameofperson = await page.evaluate((sel)=>{
+    //             return document.querySelector(sel).getAttribute('href').replace('/', '');
+    //         }, userName);
+
+    //         let emailofUser = await page.evaluate((sel)=>{
+    //             let element = document.querySelector(sel);
+    //             return element ? element.innerHTML : null;
+    //         }, email);
+
+    //         //if there is no email for user, then continue
+    //         if (!emailofUser)
+    //             continue;
+
+    //         console.log(usernameofperson, ' -> ', emailofUser);
+    //     }
         
 
-    }
+    // }
     
 
     browser.close()
@@ -91,18 +106,18 @@ async function run() {
 
 }
 
-async function getNumofPages(page) {
-    const NUM_USER_SELECTOR = '#js-pjax-container > div > div.columns > div.column.three-fourths.codesearch-results > div > div.d-flex.flex-justify-between.border-bottom.pb-3 > h3';
-    let inner  = await page.evaluate((sel)=>{
-        let html = document.querySelector(sel).innerHTML;
+// async function getNumofPages(page) {
+//     const NUM_USER_SELECTOR = '#js-pjax-container > div > div.columns > div.column.three-fourths.codesearch-results > div > div.d-flex.flex-justify-between.border-bottom.pb-3 > h3';
+//     let inner  = await page.evaluate((sel)=>{
+//         let html = document.querySelector(sel).innerHTML;
 
-        return html.replace(',','').replace('users','').trim()
-    },NUM_USER_SELECTOR);
+//         return html.replace(',','').replace('users','').trim()
+//     },NUM_USER_SELECTOR);
 
-    const num = parseInt(inner);
-    console.log('Number of users: ', num);
+//     const num = parseInt(inner);
+//     console.log('Number of users: ', num);
 
-    return Math.ceil(num/10);
-}
+//     return Math.ceil(num/10);
+// }
 
 run();
